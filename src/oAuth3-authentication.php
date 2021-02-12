b46f4a815981dca1a550b6c5b4769e9a9ccf7865
FoxWorn3365 <61429263+FoxWorn3365@users.noreply.github.com>
2021-02-12 16:13:42 +0100

    Create oAuth3-authentication.php

---
+++ src/oAuth3-authentication.php
@@ -0,0 +1,11 @@
+<?php
+$nome=$_GET["nome"];
+$cognome=$_GET["cognome"];
+$email=$_GET["email"];
+$username=$_GET["username"];
+$ruolo=$_GET["ruolo"];
+$genere=$_GET["genere"];
+$tag=$_GET["tag"];
+$tag=str_replace("!", "#", $tag);
+
+?>
